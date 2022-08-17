51d71bf6177bd9e297a5038098188461c2e9d0f0
thadydiaz@gmail.com <thadydiaz@gmail.com>
2022-08-17 00:38:58 -0500

    Introducción a java

---
+++ 4. java/java-01/Prueba1/src/main/java/HolaMundo.java
@@ -0,0 +1,7 @@
+public class HolaMundo {
+    public static void main(String[] args) {
+        String miVariable = "Hello from the other side";
+        String miNombre = "Thady";
+        System.out.println(miVariable + miNombre);
+    }
+}
---
+++ 4. java/java-01/ejemplo-jdk/Hola.java
@@ -0,0 +1,12 @@
+public class Hola {
+//esto es una clase del JDK Java
+
+	//String miVariable = "Hello from the other side";
+
+	public static void main (String arg []){
+	//es la declaracion del metodo main
+		//String miVariable = "Hello from the other side";
+		String miNombre = "Ana Thady";
+		System.out.println("Hola Saludos de:" + miNombre);
+	}
+}
