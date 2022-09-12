947ba2855cf5fe36f8c82c40a25a515e031a164b
thadydiaz@gmail.com <thadydiaz@gmail.com>
2022-09-12 17:50:14 -0500

    Introduccion a inyeccion de dependencias

---
+++ 7.Spring/Hola/src/main/java/com/generation/ahs/HolaMundo.java
@@ -0,0 +1,18 @@
+package com.generation.ahs;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RequestMapping("/")
+@RestController
+
+public class HolaMundo {
+	/*@GetMapping("/index");
+	
+	public String hola() {
+		return "index";
+	}
+	*/
+}
---
+++ 7.Spring/ejemploMVC/src/test/java/com/generation/mvc/EjemploMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
